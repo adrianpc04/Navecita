fabb0754387b6d1987db9178deb79e2d468005a5
Adrián <adrianpc04@gmail.com>
2024-12-16 21:42:30 +0100

    Acabao
    Acabao

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Meteor : MonoBehaviour
 {
     [SerializeField] private float force = 1;
-    private SpriteRenderer sprite;
-
-    private void Awake()
+    private Vector2 cero;
+    [SerializeField] GameObject over;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip boom;
+    private CircleCollider2D box;
+    private SpriteRenderer ventana;
+    private void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
+        ventana = GetComponent<SpriteRenderer>();
+        box = GetComponent<CircleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
@@ -19,13 +27,20 @@ public class Meteor : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
        if  (collision.gameObject.CompareTag("Player")){
+            audioSource.PlayOneShot(boom);
+            ventana.enabled = false;
+            box.enabled = false;
             Destroy(collision.gameObject);
             Time.timeScale = 0f;
+            Instantiate(over, cero , Quaternion.identity);
         }
         if (collision.gameObject.CompareTag("Laser"))
         {
+            audioSource.PlayOneShot(boom);
             Destroy(collision.gameObject);
-            Destroy(gameObject);
+            ventana.enabled = false;
+            box.enabled = false;
+            Destroy(gameObject,1);
         }
         if (collision.gameObject.CompareTag("MataMeteoros"))
         {
